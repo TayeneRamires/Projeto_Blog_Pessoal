c4f6c5ea83663ed8e0ee14d0384682f6f5a232a3
Tayene Ramires <tayene000@gmail.com>
2025-12-15 22:06:31 -0300

    Documentação implementada

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Indicamos a configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact("Tayene Ramires", "https://github.com/TayeneRamires", "tayene000@gmail.com")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   // Ajustando o Fuso Horário do BD
   process.env.TZ = '-03:00';
@@ -11,7 +24,7 @@ async function bootstrap() {
   // Aplicando os recursos de validação
   app.useGlobalPipes(new ValidationPipe());
 
-  // Habilitando o CORS do projeto
+  // Habilitando o CORS do projeto - controla quem pode acessar a API
   app.enableCors();
 
   // Indico qual porta o projeto está sendo executado
